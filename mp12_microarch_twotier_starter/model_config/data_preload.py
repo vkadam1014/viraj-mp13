17989cb8e6acc89576495ec7551d6be5d633f046
vkadam1014 <virajk063@gmail.com>
2026-05-12 22:19:09 -0400

    data preload

--- mp12_microarch_twotier_starter/model_config/data_preload.py
+++ mp12_microarch_twotier_starter/model_config/data_preload.py
@@ -1,5 +1,17 @@
+import argparse
 import torchvision.datasets as dsets
 
 if __name__ == "__main__":
-    dsets.MNIST(root='./data', download=True)
-    dsets.KMNIST(root='./data', download=False )
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", default="mnist")
+    args = parser.parse_args()
+
+    dataset = args.dataset.lower()
+
+    if dataset == "mnist":
+        dsets.MNIST(root="./data", train=True, download=True)
+        dsets.MNIST(root="./data", train=False, download=True)
+
+    elif dataset == "kmnist":
+        dsets.KMNIST(root="./data", train=True, download=False)
+        dsets.KMNIST(root="./data", train=False, download=False)
