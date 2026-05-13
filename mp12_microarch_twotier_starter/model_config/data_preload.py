9c1e1c3269cbd58e384fa9cab8b35f04e314d24b
vkadam1014 <virajk063@gmail.com>
2026-05-12 21:55:43 -0400

    full mp13 implementation

--- mp12_microarch_twotier_starter/model_config/data_preload.py
+++ mp12_microarch_twotier_starter/model_config/data_preload.py
@@ -2,4 +2,4 @@ import torchvision.datasets as dsets
 
 if __name__ == "__main__":
     dsets.MNIST(root='./data', download=True)
-    dsets.KMNIST(root='./data', download=True)
+    dsets.KMNIST(root='./data', download=False )
